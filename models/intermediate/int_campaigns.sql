8b78db836c769114970421cda727fae42b604a1f
William Jomaa <william@jomaa.fr>
2025-02-12 13:39:37 +0000

    int table creat

---
+++ models/intermediate/int_campaigns.sql
@@ -0,0 +1,12 @@
+-- int_campaigns.sql
+ SELECT *
+ FROM {{ref('stg_raw__adwords')}}
+ UNION ALL
+ SELECT *
+ FROM {{ref('stg_raw__bing')}}
+ UNION ALL
+ SELECT *
+ FROM {{ref('stg_raw__criteo')}}
+ UNION ALL
+ SELECT *
+ FROM {{ref('stg_raw__facebook')}}
