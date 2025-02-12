4220c4a728afdff24ae68fe36b2314d7aef77339
William Jomaa <william@jomaa.fr>
2025-02-12 10:49:49 +0000

    up

--- models/staging/raw/stg_raw__sales.sql
+++ models/staging/raw/stg_raw__sales.sql
@@ -2,18 +2,16 @@ with
 
 source as (
 
-    select * from {{ source('gz_raw_data', 'raw_gz_sales') }}
+    select * from {{ source('raw', 'sales') }}
 
 ),
-
-
 
 renamed as (
 
     select
         date_date,
         orders_id,
-        pdt_id AS products_id,
+        pdt_id,
         revenue,
         quantity
 
